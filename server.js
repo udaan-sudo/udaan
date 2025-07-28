581bbff214d51ac8abf6e22fed0b5bbdf17429e7
Sarvagra <sarvagras977@gmail.com>
2025-07-28 14:15:09 +0530

    A

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const path = require('path')
-const port = 3030
+const port = 8080
 
 const app = express();
 app.use(express.static(path.join(__dirname, 'frontend')));
